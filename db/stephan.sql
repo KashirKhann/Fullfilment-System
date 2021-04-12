137343c8f61fbeecc930f3abcd2da7e12b42e445
Kashir Khan <64606427+KashirKhann@users.noreply.github.com>
2021-04-12 10:17:18 +0500

    Update stephan.sql

--- db/stephan.sql
+++ db/stephan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2021 at 08:44 PM
+-- Generation Time: Apr 12, 2021 at 06:59 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -32,6 +32,7 @@ CREATE TABLE `customer_user` (
   `firstname` varchar(255) DEFAULT NULL,
   `lastname` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
+  `user_group` varchar(255) NOT NULL,
   `company_name` varchar(255) DEFAULT NULL,
   `street1` varchar(255) DEFAULT NULL,
   `street2` varchar(255) DEFAULT NULL,
@@ -45,8 +46,62 @@ CREATE TABLE `customer_user` (
   `password` varchar(255) DEFAULT NULL,
   `recovery_ref` varchar(255) DEFAULT NULL,
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
-  `which_admin` varchar(255) DEFAULT NULL
+  `which_admin` varchar(255) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer_user`
+--
+
+INSERT INTO `customer_user` (`id`, `firstname`, `lastname`, `email`, `user_group`, `company_name`, `street1`, `street2`, `city`, `state`, `zip_code`, `country`, `language`, `profile_avatar`, `profile_avatar_remove`, `password`, `recovery_ref`, `date_added`, `which_admin`, `created_by`) VALUES
+(1, 'Stephan', 'Raid', 'stephan@admin.com', 'SuperAdmin', '', '', '', '', '', '', '', '', '', '', 'admin@123', NULL, '2021-03-27 17:07:36', '1', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log_session`
+--
+
+CREATE TABLE `log_session` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT current_timestamp(),
+  `type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `log_session`
+--
+
+INSERT INTO `log_session` (`id`, `firstname`, `email`, `role`, `user_id`, `created_on`, `type`) VALUES
+(1, 'Stephan', 'stephan@admin.com', 'SuperAdmin', 1, '2021-04-12 09:58:38', 'login');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log_user`
+--
+
+CREATE TABLE `log_user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `which_admin` varchar(255) NOT NULL,
+  `created_by` int(111) NOT NULL,
+  `customer_user_id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `log_user`
+--
+
+INSERT INTO `log_user` (`id`, `email`, `which_admin`, `created_by`, `customer_user_id`, `type`, `created_on`) VALUES
+(1, 'kashir123@gmail.com', '1', 1, 2, 'add', '2021-04-12 09:58:59');
 
 --
 -- Indexes for dumped tables
@@ -59,6 +114,18 @@ ALTER TABLE `customer_user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `log_session`
+--
+ALTER TABLE `log_session`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `log_user`
+--
+ALTER TABLE `log_user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -66,7 +133,19 @@ ALTER TABLE `customer_user`
 -- AUTO_INCREMENT for table `customer_user`
 --
 ALTER TABLE `customer_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `log_session`
+--
+ALTER TABLE `log_session`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `log_user`
+--
+ALTER TABLE `log_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
